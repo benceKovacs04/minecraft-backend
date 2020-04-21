ed032e631b1a198725c03c7e3e93ea5f43e7d1c5
Bence Kovács <kovacs.ben.p@gmail.com>
2020-04-21 12:56:03 +0200

    added connection mapping

---
+++ MinecraftWebSocket/Interfaces/IConnectionMapping.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinecraftWebSocket.Interfaces
+{
+    interface IConnectionMapping
+    {
+        void AddConnection(string username, string connectionId);
+
+        IEnumerable<string> GetConnectionIds(string username);
+
+        void RemoveConnectionIds(string username);
+    }
+}
---
+++ MinecraftWebSocket/Services/ConnectionMappingService.cs
@@ -0,0 +1,54 @@
+﻿using MinecraftWebSocket.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinecraftWebSocket.Services
+{
+    public class ConnectionMappingService : IConnectionMapping
+    {
+        private readonly Dictionary<string, HashSet<string>> _connections = new Dictionary<string, HashSet<string>>();
+
+        public void AddConnection(string username, string connectionId)
+        {
+            lock (_connections)
+            {
+                HashSet<string> connections;
+                if(!_connections.TryGetValue(username, out connections))
+                {
+                    connections = new HashSet<string>();
+                    _connections.Add(username, connections);
+                }
+
+                lock (connections)
+                {
+                    connections.Add(connectionId);
+                }
+
+            }
+        }
+
+        public IEnumerable<string> GetConnectionIds(string username)
+        {
+            HashSet<string> connections;
+            if(_connections.TryGetValue(username, out connections))
+            {
+                return connections;
+            }
+
+            return Enumerable.Empty<string>();
+        }
+
+        public void RemoveConnectionIds(string username)
+        {
+            lock (_connections)
+            {
+                if(_connections.ContainsKey(username))
+                {
+                    _connections.Remove(username);
+                }
+            }
+        }
+    }
+}
